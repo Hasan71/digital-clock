d7e23e682c57384d7741eb4b021d304170acd84e
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-10-03 23:59:53 +0600

    added js

---
+++ script.js
@@ -0,0 +1,13 @@
+// function myTime () {
+//     let myDate = new Date();
+//     let hour, minute = (myDate.getMinutes() <); 
+// }
+
+function myFunction (a, b) {
+    console.log('sure?');
+    return  a * b ;
+}
+
+let ok = myFunction;
+
+console.log(ok);
