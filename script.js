38c310c428200b2f036bfc562f467c0e92046006
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-10-05 23:59:39 +0600

    added completed js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-// function myTime () {
-//     let myDate = new Date();
-//     let hour, minute = (myDate.getMinutes() <); 
-// }
-
-function myFunction (a, b) {
-    console.log('sure?');
-    return  a * b ;
+function myTime () {
+    let myDate = new Date();
+    let hour, minute = (myDate.getMinutes() < 10) ? "0" + myDate.getMinutes() : myDate.getMinutes(),
+        second = (myDate.getSeconds() < 10) ? "0" + myDate.getUTCSeconds() : myDate.getSeconds(),
+        M = (myDate.getHours() >= 12) ? "PM" : "AM";  
 }
 
-let ok = myFunction;
 
-console.log(ok);
